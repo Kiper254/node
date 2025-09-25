a70a637770610b192dc4ad6124bca0314e4a9296
Kacper Labudda <kiper2541@gmail.com>
2025-09-25 08:18:52 +0200

    Name w consoli

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const name = "Kacper";
 app.get('/', (req, res) => {
   res.send('Działa Express!');
 });
 
 app.listen(port, () => {
   console.log(`Serwer Express działa na http://localhost:${port}`);
+  console.log(`Mam na imie ${name}`);
 });
